a59e3adfcd684ad3c18ce3700cf4ee643289352b
Adem Çeper <120774440+ademceper@users.noreply.github.com>
2023-07-16 14:39:14 +0300

    adem

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -7,4 +7,15 @@ Console.WriteLine(isim);
 
 
 //STRİNG
+string deneme = "Adem ÇEPER";       //string değişkene değer atıyoruz
+Console.WriteLine(deneme);
+
+deneme = "Hello World";              //Değişkenin değerini değiştirip tekrar yazdırdığımızda denemede yeni değer bulunuyor.
+Console.WriteLine(deneme);
+
+string buyukKarakter = deneme.ToUpper();      //Karakterleri büyültmek için kullanılan metod
+Console.WriteLine(buyukKarakter);
+
+string kucukKarakter = deneme.ToLower();       //Karakterleri küçültmek için kullanılan metod
+Console.WriteLine(kucukKarakter);
 
