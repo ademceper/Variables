632b0904473e106aaa044304a408b3dbb0b3b044
Adem Çeper <120774440+ademceper@users.noreply.github.com>
2023-07-16 13:04:47 +0300

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿string isimSoyisim = "Adem ÇEPER"; // Değişken Tanımlama Örneği
+Console.WriteLine(isimSoyisim);   //Konsola Çıktı Örneği
+
+string isim;                         //İkinci tanımlama yönetimi
+isim = "Adem ÇEPER";                 //Devamı                           
+Console.WriteLine(isim);
+
+
+//STRİNG
+
